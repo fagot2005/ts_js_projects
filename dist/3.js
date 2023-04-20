6405c8d8a6bac8a1e0d868a3c2577c6a127da985
fagot2005 <v.lashchenko.1976@gmail.com>
2023-04-20 12:02:57 +0300

    Add enum

---
+++ dist/3.js
@@ -0,0 +1,10 @@
+"use strict";
+//Numeric Enum
+//Variable enum - Pac
+//console.log(RollNumber);
+let studentsList1 = 2 /* RollNumber.Ivan */;
+let studentsList2 = 1 /* RollNumber.Bil */;
+let studentsList3 = 0 /* RollNumber.Jonn */;
+console.log(studentsList1);
+console.log(studentsList2);
+console.log(studentsList3);
---
+++ src/3.ts
@@ -0,0 +1,19 @@
+//Numeric Enum
+//Variable enum - Pac
+
+const enum RollNumber {
+  Jonn,
+  Bil,
+  Ivan,
+  Petro,
+}
+
+//console.log(RollNumber);
+
+let studentsList1: RollNumber = RollNumber.Ivan;
+let studentsList2: RollNumber = RollNumber.Bil;
+let studentsList3: RollNumber = RollNumber.Jonn;
+
+console.log(studentsList1);
+console.log(studentsList2);
+console.log(studentsList3);
