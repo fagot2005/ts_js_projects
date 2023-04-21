c7a41927a1f0f4a93dc4bcaf92d930b50942a805
fagot2005 <v.lashchenko.1976@gmail.com>
2023-04-21 11:51:11 +0300

    Many different Loop

---
+++ src/5-For-Loops.ts
@@ -0,0 +1,32 @@
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+let numArray: (string | number)[] = [
+  "One",
+  10,
+  20,
+  "Two",
+  30,
+  50,
+  "Five",
+  60,
+  70,
+  80,
+  90,
+];
+
+// for (let i: number = 0; i < numArray.length; i++) {
+//   console.log(numArray[i]);
+// }
+
+for (let item of numArray.entries()) {
+  console.log(item[0], item[1]);
+}
+
+console.log("==============================");
+
+for (let [i, item] of numArray.entries()) {
+  //console.log(item[0], item[1]);
+  console.log(`${i+1} - ${item}`);
+}
