2b1bc8bf3c0c44681761fb623f75923ef3084f6f
fagot2005 <v.lashchenko.1976@gmail.com>
2023-04-21 09:53:58 +0300

    Add new Etum type

--- src/3.ts
+++ src/3.ts
@@ -17,3 +17,37 @@ let studentsList3: RollNumber = RollNumber.Jonn;
 console.log(studentsList1);
 console.log(studentsList2);
 console.log(studentsList3);
+
+//String Enum type
+
+const enum StudentsDetails {
+  Name = "Bil",
+  Major = "Teacher",
+}
+
+//console.log(RollNumber);
+
+let studentsName: StudentsDetails = StudentsDetails.Name;
+let studentsMajor: StudentsDetails = StudentsDetails.Major;
+
+console.log(studentsName, studentsMajor);
+
+//Heterogeneouus Enum
+
+const enum StudentsNewDetails {
+  ID = 1,
+  Name = "Bil",
+  Major = "Teacher",
+}
+
+//console.log(RollNumber);
+
+let studentsNewID: StudentsNewDetails = StudentsNewDetails.ID;
+let studentsNewName: StudentsNewDetails = StudentsNewDetails.Name;
+let studentsNewMajor: StudentsNewDetails = StudentsNewDetails.Major;
+
+console.log(studentsNewID, studentsName, studentsMajor);
+console.log(
+  `${studentsNewID} - ${studentsName} - has a major in ${studentsMajor}`
+);
+
