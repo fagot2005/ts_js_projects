bcdf6ffbd9d87d15ff950dc77bf224c68d433060
fagot2005 <v.lashchenko.1976@gmail.com>
2023-04-25 23:19:56 +0300

    new Object

--- dist/9-Object.js
+++ dist/9-Object.js
@@ -5,3 +5,7 @@ const myCar = {
     brand: "BMW",
     power: 95,
 };
+console.log(`${myCar.brand} - this is my ${myCar.color}, and this is ${myCar.countOfDors} and ${myCar.power}`);
+myCar.power = 230;
+console.log("=====================================");
+console.log(`${myCar.brand} - this is my ${myCar.color}, and this is ${myCar.countOfDors} and ${myCar.power}`);
--- src/9-Object.ts
+++ src/9-Object.ts
@@ -12,3 +12,15 @@ const myCar: Car = {
   brand: "BMW",
   power: 95,
 };
+
+console.log(
+  `${myCar.brand} - this is my ${myCar.color}, and this is ${myCar.countOfDors} and ${myCar.power}`
+);
+
+myCar.power = 230;
+
+console.log("=====================================");
+
+console.log(
+  `${myCar.brand} - this is my ${myCar.color}, and this is ${myCar.countOfDors} and ${myCar.power}`
+);
