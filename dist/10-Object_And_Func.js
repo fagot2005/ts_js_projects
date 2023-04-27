f1b6954d327fe90f0d9bd0922410b1e9cdb2cc13
fagot2005 <v.lashchenko.1976@gmail.com>
2023-04-27 15:26:24 +0300

    Update

--- dist/10-Object_And_Func.js
+++ dist/10-Object_And_Func.js
@@ -2,6 +2,9 @@
 function printPoint(point) {
     console.log(`Our coordinate x=${point.x}, and y=${point.y}`);
 }
+function printUser(user) {
+    console.log(`Your first name - ${user.firstName.toUpperCase()}, and last name - ${user.lastName.toUpperCase()}`);
+}
 const myPoint1 = {
     x: 200,
     y: 300,
@@ -11,5 +14,16 @@ const myPoint2 = {
     y: 270,
     z: 560,
 };
+const user1 = {
+    firstName: "Petro",
+    lastName: "Petrov",
+};
+const user2 = {
+    firstName: "Dmitro",
+    lastName: "Dmitrenko",
+};
 printPoint(myPoint1);
 printPoint(myPoint2);
+console.log("=========================================================================");
+printUser(user1);
+printUser(user2);
--- src/10-Object_And_Func.ts
+++ src/10-Object_And_Func.ts
@@ -2,9 +2,21 @@ function printPoint(point: { x: number; y: number }): void {
   console.log(`Our coordinate x=${point.x}, and y=${point.y}`);
 }
 
+
+function printUser(user: { firstName: string; lastName: string }): void {
+  console.log(
+    `Your first name - ${user.firstName.toUpperCase()}, and last name - ${user.lastName.toUpperCase()}`
+  );
+}
+
 interface Point {
   x: number;
   y: number;
+}
+
+interface User {
+  firstName: string;
+  lastName: string;
 }
 
 const myPoint1: Point = {
@@ -18,5 +30,18 @@ const myPoint2 = {
   z: 560,
 };
 
+const user1: User = {
+  firstName: "Petro",
+  lastName: "Petrov",
+};
+
+const user2: User = {
+  firstName: "Dmitro",
+  lastName: "Dmitrenko",
+};
+
 printPoint(myPoint1);
 printPoint(myPoint2);
+console.log("=========================================================================")
+printUser(user1);
+printUser(user2);
